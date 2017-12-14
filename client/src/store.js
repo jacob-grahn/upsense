aa6fa6907a946abc4934d62f9185000613bc6bdd
Jacob Grahn <contact@jacobgrahn.com>
2017-12-13 23:16:46 -0500

    auto generate ws address

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,7 @@ import * as jiber from 'jiber-client'
 import reducer from '../../shared/reducer'
 import env from './env'
 
-const url = 'ws:\\localhost:3000'
+const url = ((window.location.protocol === 'https:') ? 'wss://' : 'ws://') + window.location.host
 const store = jiber.createStore({ url, reducer })
 const room = store.createRoom(env.roomId)
 export { store, room }

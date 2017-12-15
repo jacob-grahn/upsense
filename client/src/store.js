a59ab78e4dbaedf32a2255aacddfcf17590d03a6
Jacob Grahn <contact@jacobgrahn.com>
2017-12-15 13:59:40 -0500

    turn off p2p and clean up some css

--- client/src/store.js
+++ client/src/store.js
@@ -2,9 +2,8 @@ import * as jiber from 'jiber-client'
 import reducer from '../../shared/reducer'
 import env from './env'
 
-// const protocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://'
-// const url = protocol + window.location.host + '/ws'
 const url = env.host.replace('https:', 'wss:').replace('http:', 'ws:') + '/ws'
-const store = jiber.createStore({ url, reducer })
+const maxPeers = 0
+const store = jiber.createStore({ url, reducer, maxPeers })
 const room = store.createRoom(env.roomId)
 export { store, room }
